8c68cbcc1faee25f2c1fe679514c5b86f8c0d56c
tom110 <254449149@qq.com>
2020-03-02 17:27:23 +0800

    添加下载后修改文件名命令，并优化下载完毕判断

--- bplz/bplz.py
+++ bplz/bplz.py
@@ -18,6 +18,18 @@ class Bplz:
     def __init__(self):
         pass
 
+    def isExist(self, dir, etentName):
+        Files = os.listdir(dir)
+        for k in range(len(Files)):
+            # 提取文件夹内所有文件的后缀
+            Files[k] = os.path.splitext(Files[k])[1]
+
+        # 你想要找的文件的后缀
+        if etentName in Files:
+            return True
+        else:
+            return False
+
     def downloadRun(self, bufferTime, url):
         directory = os.path.join(tempfile.gettempdir(), str(uuid.uuid1()))
         if not os.path.exists(directory):
@@ -29,10 +41,10 @@ class Bplz:
         firefox_options = Options()
         firefox_options.add_argument('--headless')
 
-        bplzdir=os.path.dirname(bplz.__file__)
+        bplzdir = os.path.dirname(bplz.__file__)
 
         browser = webdriver.Firefox(options=firefox_options,
-                                    executable_path=os.path.join(bplzdir,"geckodriver.exe"))
+                                    executable_path=os.path.join(bplzdir, "geckodriver.exe"))
         browser.get(url)
 
         time.sleep(bufferTime)
@@ -48,7 +60,8 @@ class Bplz:
 
             req = request.Request(readyUrl)
             req.add_header("User-Agent",
-                           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3080.5 Safari/537.36")
+                           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                           "Chrome/60.0.3080.5 Safari/537.36")
 
             profile = webdriver.FirefoxProfile()
             profile.set_preference('browser.download.dir', directory)
@@ -59,7 +72,7 @@ class Bplz:
                                    'application/octet-stream')  # 对应header里面的content-type
 
             browser1 = webdriver.Firefox(firefox_profile=profile, options=firefox_options,
-                                        executable_path=os.path.join(bplzdir,"geckodriver.exe")) 
+                                         executable_path=os.path.join(bplzdir, "geckodriver.exe"))
 
             try:
                 response = request.urlopen(req, timeout=10)
@@ -72,27 +85,22 @@ class Bplz:
                 time.sleep(bufferTime)
                 browser1.find_element_by_tag_name('a').click()
                 print("开始下载链接：" + downloadPageUrl)
-
-                # browser.get(downloadPageUrl)
-                # downloadElem = browser.find_element_by_id('go')
-                # time.sleep(1)
-                # downloadFileUrl = BeautifulSoup(downloadElem.get_attribute('innerHTML'), 'html.parser').contents[0].attrs['href']
-                #
-                # print(downloadFileUrl)
-
             except:
                 print("文件获取失败")
+                p = Popen("taskkill /IM firefox.exe /F", stdout=PIPE, stderr=STDOUT)
                 sys.exit()
-
             finally:
                 browser1.close()
 
         while len([lists for lists in os.listdir(directory) if os.path.isfile(os.path.join(directory, lists))]) < len(
-                readys):
+                readys) \
+                or self.isExist(directory, ".part"):
+            print("等待文件下载完成。。。。" +
+                  str(len([lists for lists in os.listdir(directory) if
+                           os.path.isfile(os.path.join(directory, lists))])) + "/" + str(len(readys)))
             time.sleep(2)
 
         print("下载完成，正进行后续处理。。。。")
-        time.sleep(4)
         browser.close()
         browser.quit()
         browser1.quit()
@@ -126,32 +134,44 @@ class Bplz:
     def main(self, argv):
         buftime = 2
         url = 'https://www.lanzous.com/b00t9lyqf'
+        renamefolder=""
 
         try:
-            opts, args = getopt.getopt(argv, "hb:u:f:", ["buftime=", "url="])
+            opts, args = getopt.getopt(argv, "hb:u:f:r:", ["buftime=", "url=","renameFolder="])
         except getopt.GetoptError:
             print('python bplz.py - b 输入网页缓冲时间，单位秒，int类型,默认2秒\n' +
-                  '-u 输入蓝奏云文件夹共享网址，注意网址不能有密码\n')
+                  '-u 输入蓝奏云文件夹共享网址，注意网址不能有密码\n'+
+                  '-r 输入蓝奏云下载文件夹路径，单独使用')
             sys.exit(2)
         for opt, arg in opts:
             if opt in ('-h', "--help"):
                 print('python bplz.py - b 输入网页缓冲时间，单位秒，int类型,默认2秒\n' +
-                      '-u 输入蓝奏云文件夹共享网址，注意网址不能有密码\n')
+                      '-u 输入蓝奏云文件夹共享网址，注意网址不能有密码\n' +
+                      '-r 输入蓝奏云下载文件夹路径，单独使用')
                 sys.exit()
             elif opt in ("-b", "--buftime"):
                 buftime = arg
             elif opt in ("-u", "--url"):
                 url = arg
+            elif opt in ("-r","--renamefolder"):
+                renamefolder= arg
 
         if len(opts) == 0:
             print('python bplz.py - b 输入网页缓冲时间，单位秒，int类型,默认2秒\n' +
-                  '-u 输入蓝奏云文件夹共享网址，注意网址不能有密码\n')
+                  '-u 输入蓝奏云文件夹共享网址，注意网址不能有密码,默认下载qgis3.4\n' +
+                  '-r 输入蓝奏云下载文件夹路径，单独使用')
             sys.exit()
-        print('网页加载缓冲时间：', buftime)
-        print('蓝奏云文件夹共享网址：', url)
 
-        directory = self.downloadRun(int(buftime), url)
-        file_name = self.getRealZip(directory)
-        print("开始修改文件名")
-        self.rename(directory, file_name)
-        print("文件下载完毕，放置位置为：" + directory)
+        if renamefolder!="":
+            print('重命名文件夹为：'+renamefolder)
+            file_name = self.getRealZip(renamefolder)
+            print("开始修改文件名")
+            self.rename(renamefolder, file_name)
+        else:
+            print('网页加载缓冲时间：'+ str(buftime))
+            print('蓝奏云文件夹共享网址：'+ url)
+            directory = self.downloadRun(int(buftime), url)
+            file_name = self.getRealZip(directory)
+            print("开始修改文件名")
+            self.rename(directory, file_name)
+            print("文件下载完毕，放置位置为：" + directory)
